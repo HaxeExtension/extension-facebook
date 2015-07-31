e1cd83f0cc6becd3849dd81374fc01d7bb4ed0de
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-07-31 17:04:45 -0300

    Adding Android support

---
+++ dependencies/android/facebook/src/org/haxe/extension/facebook/FacebookExtension.java
@@ -0,0 +1,12 @@
+package org.haxe.extension.facebook;
+
+import org.haxe.extension.Extension;
+import org.haxe.lime.HaxeObject;
+
+public class FacebookExtension extends Extension {
+
+	public static void init(HaxeObject callbackObject,String gId,boolean test,boolean debug){
+		System.out.println("Init");
+	}
+	
+}
