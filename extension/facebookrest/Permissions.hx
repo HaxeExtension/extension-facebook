4686ccf45ce398f410ee1543ab892c082af3d2bb
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-08-20 12:47:46 -0300

    Added a method to ask for current permissions

---
+++ extension/facebookrest/Permissions.hx
@@ -0,0 +1,37 @@
+package extension.facebookrest;
+
+import haxe.Json;
+
+enum Status {
+	Granted;
+	Declined;
+}
+typedef PermissionStatus = { permission : String, status : Status }
+
+class Permissions {
+
+	public static function currentPermissions(
+		f : Facebook,
+		onSuccess : Array<PermissionStatus>->Void,
+		onError : Dynamic->Void
+	) : Void {
+
+		f.get(
+			"/me/permissions",
+			function(data) {
+				var arr = [];
+				for (json in cast(data.data, Array<Dynamic>)) {
+					var ps : PermissionStatus = { 
+						permission : json.permission,
+						status: json.status=="granted" ? Granted : Declined
+					};
+					arr.push(ps);
+				}
+				onSuccess(arr);
+			},
+			onError
+		);
+
+	}
+
+}
