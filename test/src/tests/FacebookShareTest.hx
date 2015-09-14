bc260e1d5524c5f094f409c8c9ebce353413d5b9
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-09-14 17:15:13 -0300

    Add "Share" test

--- test/src/tests/FacebookShareTest.hx
+++ test/src/tests/FacebookShareTest.hx
@@ -12,52 +12,18 @@ import flash.Lib;
 import flash.text.TextField;
 import haxe.unit.TestCase;
 
-class FacebookTest extends TestCase {
+class FacebookShareTest extends TestCase {
 
 	var spr : Sprite;
 	var face : Facebook;
-	var tokenTxt : TextField;
-
-	function printFun(str : String) {
-		#if mobile
-		trace(str);
-		#else
-		Sys.println(str);
-		#end
-	}
 
 	function onLoggedIn() {
-		tokenTxt.text = face.accessToken;
-		/*
-		FriendList.invitableFriends(
-			face,
-			function(friends : Array<UserInvitableFriend>) {
-				for (f in friends) {
-					printFun(f.name);
-				}
-			},
-			printFun
+		Share.link(
+			"http://www.sempaigames.com/daktylos",
+			"El Daktylooos",
+			"http://www.sempaigames.com/images/daktylos/daktylos-poster-2.jpg",
+			"Juega al Daktylos, un divertido juego prehistorico =)"
 		);
-		*/
-		/*
-		AppInvite.setOnCompleteCallback(printFun);
-		AppInvite.setOnFailCallback(printFun);
-		AppInvite.invite("https://fb.me/1654475341456363");
-		*/
-		AppRequests.setOnCompleteCallback(function(d) trace(d));
-		AppRequests.setOnFailCallback(printFun);
-		AppRequests.getObjectList(face, function(d) trace('result: $d'), printFun);
-		/*
-		AppRequests.gameRequestSend({
-			message : "Mensaje de test",
-			title : "Titleloko",
-			objectId : "1494850427480255",
-			actionType : GameRequestActionType.Send
-		});
-		*/
-
-		Permissions.currentPermissions(face, function(a) trace(a), function(e) trace(e));
-
 	}
 
 	public function test() {
@@ -68,11 +34,6 @@ class FacebookTest extends TestCase {
 		gfx.drawRect(0, 0, 200, 200);
 		gfx.endFill();
 		Lib.current.stage.addChild(spr);
-
-		tokenTxt = new TextField();
-		tokenTxt.autoSize = flash.text.TextFieldAutoSize.LEFT;
-		tokenTxt.scaleX = tokenTxt.scaleY = 2.0;
-		Lib.current.stage.addChild(tokenTxt);
 
 		Lib.current.stage.addEventListener(Event.ENTER_FRAME, onEnterFrame);
 
