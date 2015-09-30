8248a0b32c48dca98bab5238a68a8a7a4967e000
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-09-30 14:05:12 -0300

    [iOS] Trying another solution for the audio issue

--- project/ios/FacebookAppDelegate.mm
+++ project/ios/FacebookAppDelegate.mm
@@ -13,24 +13,76 @@
 									annotation:annotation];
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-	[[AVAudioSession sharedInstance] setActive:NO error:nil];
+// Fix audio issue:
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	NSLog(@"%s", __FUNCTION__);
+	[self startAudio];
+	return YES;
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-	[[AVAudioSession sharedInstance] setActive:NO error:nil];
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+	NSLog(@"%s", __FUNCTION__);
+
+	// SpriteKit uses AVAudioSession for [SKAction playSoundFileNamed:]
+	// AVAudioSession cannot be active while the application is in the background,
+	// so we have to stop it when going in to background
+	// and reactivate it when entering foreground.
+	// This prevents audio crash.
+	[self stopAudio];
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-	[[AVAudioSession sharedInstance] setActive:YES error:nil];
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+	NSLog(@"%s", __FUNCTION__);
+
+	[self startAudio];
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-	[[AVAudioSession sharedInstance] setActive:YES error:nil];
+- (void)applicationWillTerminate:(UIApplication *)application {
+	NSLog(@"%s", __FUNCTION__);
+
+	[self stopAudio];
+}
+
+static BOOL isAudioSessionActive = NO;
+
+- (void)startAudio {
+	AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+	NSError *error = nil;
+
+	if (audioSession.otherAudioPlaying) {
+		[audioSession setCategory: AVAudioSessionCategoryAmbient error:&error];
+	} else {
+		[audioSession setCategory: AVAudioSessionCategorySoloAmbient error:&error];
+	}
+
+	if (!error) {
+		[audioSession setActive:YES error:&error];
+		isAudioSessionActive = YES;
+	}
+
+	NSLog(@"%s AVAudioSession Category: %@ Error: %@", __FUNCTION__, [audioSession category], error);
+}
+
+- (void)stopAudio {
+	// Prevent background apps from duplicate entering if terminating an app.
+	if (!isAudioSessionActive) return;
+
+	AVAudioSession *audioSession = [AVAudioSession sharedInstance];
+	NSError *error = nil;
+
+	[audioSession setActive:NO error:&error];
+
+	NSLog(@"%s AVAudioSession Error: %@", __FUNCTION__, error);
+
+	if (error) {
+		// It's not enough to setActive:NO
+		// We have to deactivate it effectively (without that error),
+		// so try again (and again... until success).
+		[self stopAudio];
+	} else {
+		isAudioSessionActive = NO;
+	}
 }
 
 // App Invite dialog callbacks:
